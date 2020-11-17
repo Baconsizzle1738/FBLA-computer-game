b3eaaee839b711db41ed5c58cae07010daf813bf
Baconsizzle1738 <zhengdundun@gmail.com>
2020-11-17 14:10:02 -0600

    added key listener if github will cooperate >:(

---
+++ ThisIsAGame/src/me/Baconsizzle1738/main/GameKeyListener.java
@@ -0,0 +1,16 @@
+package me.Baconsizzle1738.main;
+
+import java.util.EventListener;
+
+public class GameKeyListener implements EventListener{
+	GameHandler handler;
+	
+	public GameKeyListener(GameHandler h) {
+		handler = h;
+	}
+	
+	
+	
+	
+	
+}
