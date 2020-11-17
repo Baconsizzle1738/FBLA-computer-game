5ac128e8a7ff5e9d8d62bea257c7e264589161a3
Baconsizzle1738 <zhengdundun@gmail.com>
2020-11-17 14:11:20 -0600

    REEEEE

--- ThisIsAGame/src/me/Baconsizzle1738/main/GameKeyListener.java
+++ ThisIsAGame/src/me/Baconsizzle1738/main/GameKeyListener.java
@@ -1,12 +1,35 @@
 package me.Baconsizzle1738.main;
 
-import java.util.EventListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+//import java.util.EventListener;
 
-public class GameKeyListener implements EventListener{
+public class GameKeyListener implements KeyListener{
 	GameHandler handler;
+	
+	//in order of w, a, s, d
+	boolean[] keys = {false,false,false,false};
 	
 	public GameKeyListener(GameHandler h) {
 		handler = h;
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 	
