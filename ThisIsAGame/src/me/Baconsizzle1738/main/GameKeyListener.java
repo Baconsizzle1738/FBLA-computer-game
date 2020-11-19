fee24f73cd14969cb0ddfe7f573a278d12e64329
Baconsizzle1738 <zhengdundun@gmail.com>
2020-11-19 00:25:43 -0600

    update keyboard input

--- ThisIsAGame/src/me/Baconsizzle1738/main/GameKeyListener.java
+++ ThisIsAGame/src/me/Baconsizzle1738/main/GameKeyListener.java
@@ -6,19 +6,13 @@ import java.awt.event.KeyListener;
 
 public class GameKeyListener implements KeyListener{
 	GameHandler handler;
-	GameObject player;
 	
 	//in order of w, a, s, d
 	boolean[] keys = {false,false,false,false};
 	
 	public GameKeyListener(GameHandler h) {
 		handler = h;
-		//searches for player ID GameObject
-		for (int i = 0; i<handler.objects.size(); i++) {
-			if (handler.objects.get(i).gettypeID()==ID.Player) {
-				player = handler.objects.get(i);
-			}
-		}
+		
 	}
 
 	@Override
@@ -29,34 +23,64 @@ public class GameKeyListener implements KeyListener{
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		if (e.getKeyCode() == KeyEvent.VK_W && keys[0] == false) {
-			player.setVolY(-5);
+		
+		for (int i = 0; i<handler.objects.size(); i++) {
+			if (handler.objects.get(i).gettypeID() == ID.Player) {
+				
+				GameObject player = handler.objects.get(i);
+				
+				if (e.getKeyCode() == KeyEvent.VK_W) {
+					player.setVolY(-5);
+					keys[0] = true;
+				}
+				if (e.getKeyCode() == KeyEvent.VK_A) {
+					player.setVolX(-5);
+					keys[1] = true;
+				}
+				if (e.getKeyCode() == KeyEvent.VK_S) {
+					player.setVolY(5);
+					keys[2] = true;
+				}
+				if (e.getKeyCode() == KeyEvent.VK_D) {
+					player.setVolX(5);
+					keys[3] = true;
+				}
+			}
 		}
-		if (e.getKeyCode() == KeyEvent.VK_A && keys[1] == false) {
-			player.setVolX(-5);
-		}
-		if (e.getKeyCode() == KeyEvent.VK_S && keys[2] == false) {
-			player.setVolY(5);
-		}
-		if (e.getKeyCode() == KeyEvent.VK_D && keys[3] == false) {
-			player.setVolX(5);
-		}
+		
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		if (e.getKeyCode() == KeyEvent.VK_W) {
-			player.setVolY(0);
+		
+		for (int i = 0; i<handler.objects.size(); i++) {
+			if (handler.objects.get(i).gettypeID() == ID.Player) {
+				
+				GameObject player = handler.objects.get(i);
+				
+				if (e.getKeyCode() == KeyEvent.VK_W) {
+					keys[0] = false;
+				}
+				if (e.getKeyCode() == KeyEvent.VK_A) {
+					keys[1] = false;
+				}
+				if (e.getKeyCode() == KeyEvent.VK_S) {
+					keys[2] = false;
+				}
+				if (e.getKeyCode() == KeyEvent.VK_D) {
+					keys[3] = false;
+				}
+				
+				
+				if (!keys[0] && !keys[2]) {
+					player.setVolY(0);
+				}
+				if (!keys[1] && !keys[3]) {
+					player.setVolX(0);
+				}
+			}
 		}
-		if (e.getKeyCode() == KeyEvent.VK_A) {
-			player.setVolX(0);
-		}
-		if (e.getKeyCode() == KeyEvent.VK_S) {
-			player.setVolY(0);
-		}
-		if (e.getKeyCode() == KeyEvent.VK_D) {
-			player.setVolX(0);
-		}
+		
 	}
 	
 }
