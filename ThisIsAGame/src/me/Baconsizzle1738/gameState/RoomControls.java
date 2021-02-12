24ec5cf45b5ac70e616127ac0ceb976918889c34
Baconsizzle1738 <zhengdundun@gmail.com>
2021-02-12 14:33:00 -0600

    controls level

---
+++ ThisIsAGame/src/me/Baconsizzle1738/gameState/RoomControls.java
@@ -0,0 +1,46 @@
+package me.Baconsizzle1738.gameState;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import me.Baconsizzle1738.Objects.Button;
+import me.Baconsizzle1738.Objects.GameHandler;
+import me.Baconsizzle1738.main.ID;
+
+public class RoomControls extends Room{
+	
+	private Button backButton = new Button(200, 200, ID.Button, lvl, "BACK", 30, 10, Color.red, new Color(160, 0, 0), new Color(200, 200, 200));
+	
+	public RoomControls(int spawnX, int spawnY, GameHandler h, int lvl) {
+		super(spawnX, spawnY, h, lvl);
+		
+		
+	}
+
+	@Override
+	public void startLevel() {
+		
+		handler.objects.add(backButton);
+	}
+
+	@Override
+	public void reset() {
+		//nothing happen
+	}
+
+	@Override
+	public boolean isComplete() {
+		return backButton.isReleased();
+	}
+
+	@Override
+	public void tick() {
+		
+	}
+
+	@Override
+	public void render(Graphics g) {
+		
+	}
+
+}
